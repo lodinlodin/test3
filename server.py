e1cf03a770fc6fe7b2668f6f3120a44897da5786
lll <mamagloria8@gmail.com>
2019-11-22 22:18:38 +0800

    2

--- server.py
+++ server.py
@@ -39,9 +39,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    foo == event.message.text
-        if foo[0] == '?':
-            pass
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
