5276d7f4acf4c7feb121441b691d13ff2d6d58d1
lll <mamagloria8@gmail.com>
2019-11-25 23:57:06 +0800

    6

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-# test3
-import os
+
+import json
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -14,9 +15,9 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('YOUR_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('YOUR_CHANNEL_SECRET'))
 
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -25,13 +26,13 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -39,7 +40,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   from linebot import LineBotApi
+    # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
+
+    from linebot import LineBotApi
     from linebot.models import TextSendMessage, ImageSendMessage
     from linebot.exceptions import LineBotApiError
 
@@ -48,14 +53,22 @@ def handle_message(event):
 
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-#文字訊息
-
     try:
-        line_bot_api.push_message(to, TextSendMessage(text='台科大電腦研習社'))
+    line_bot_api.push_message(to, TextSendMessage(text='台科大電腦研習社'))
+    except LineBotApiError as e:
+    # error handle
+        raise e
+        line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+        image_url = "https://i.imgur.com/eTldj2E.png?1"
+    try:
+        line_bot_api.push_message(to, ImageSendMessage(original_content_url=image_url,  preview_image_url=image_url))
     except LineBotApiError as e:
     # error handle
         raise e
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+@app.route('/')
+def homepage():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
