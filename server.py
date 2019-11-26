6e73fd6fd7923b4d96ba6ffb6c1c6ccdcf4af276
lll <mamagloria8@gmail.com>
2019-11-26 13:52:44 +0800

    tj/6 9

--- server.py
+++ server.py
@@ -44,7 +44,6 @@ def handle_message(event):
          line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     elif event.message.text == "貼圖":
         line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=1, sticker_id=2))
-    elif event.message.text == "圖片": line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg', preview_image_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg'))
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    elif event.message.text == "圖片": 
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg', preview_image_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg'))
+    
