e1e8fd8f283d890ac20460d2b7497636f7bfc990
lll <mamagloria8@gmail.com>
2019-11-26 14:01:43 +0800

    tj/6yji4 10

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-# test3
-import os
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -8,42 +7,53 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+from linebot.models import  (
+    MessageEvent, TextMessage, TextSendMessage,StickerSendMessage,ImageSendMessage
 )
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('YOUR_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('YOUR_CHANNEL_SECRET'))
 
+line_bot_api = LineBotApi('YOUR_Channel_access_token')
+handler = WebhookHandler('YOUR_Channel_secret')
 
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route("/", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
+    print("Request body: " + body, "Signature: " + signature)
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+       abort(400)
 
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):   
-    msg = msg.encode('utf-8')
-    if event.message.text == '文字'
-         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+def handle_message(event):
+    msg = event.message.text
+    #print(type(msg))
+    msg = msg.encode('utf-8')  
+    if event.message.text == "文字":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     elif event.message.text == "貼圖":
         line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=1, sticker_id=2))
-    elif event.message.text == "圖片": 
+    elif event.message.text == "圖片":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg', preview_image_url='https://miro.medium.com/max/534/1*icRYHYAJJQOJbYznJbtO7g.jpeg'))
-    
+    elif event.message.text == "影片":
+        line_bot_api.reply_message(event.reply_token,VideoSendMessage(original_content_url='影片網址', preview_image_url='預覽圖片網址'))
+    elif event.message.text == "音訊":
+        line_bot_api.reply_message(event.reply_token,AudioSendMessage(original_content_url='音訊網址', duration=100000))
+    return 'OK2'
+
+if __name__ == "__main__":
+    app.run(debug=True,port=80)
