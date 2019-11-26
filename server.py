650f196acebdc75e06290c0c32df416961635fda
lll <mamagloria8@gmail.com>
2019-11-26 13:33:34 +0800

    tu/6 7

--- server.py
+++ server.py
@@ -41,7 +41,3 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
-    app.run(debug=True,port=80)
