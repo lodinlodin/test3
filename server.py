3c07038de2504bc77f06d587ca0ce076d4625b6f
lll <mamagloria8@gmail.com>
2020-06-14 14:34:15 +0800

    再次嘗試喔

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# lee
+
 import os
 from flask import Flask, request, abort
 
@@ -8,9 +8,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
-)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, StickerSendMessage)
 
 app = Flask(__name__)
 
@@ -27,7 +25,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
  
-    # handle webhook body
+    # handle webhook bodyeeee
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -37,11 +35,27 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):   
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
-    event.message.text == "貼圖":
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=11537, sticker_id=52002734))
+    # 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
